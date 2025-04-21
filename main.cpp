73ff6bbe60ca157a0d81db1dbc1bc6e64d5a6f28
AngelaAcquista <acquistaangela@gmail.com>
2025-04-21 19:10:40 -0400

    adding texture load to proj

--- main.cpp
+++ main.cpp
@@ -1,21 +1,48 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <map>
+#include <string>
+#include <vector>
+#include <fstream>
+
+using std::map;
+using std::string;
+using std::vector;
+using std::cerr;
+using std::endl;
+using sf::Texture;
+using std::ifstream;
+using sf::RenderWindow;
+using std::to_string;
+using sf::Event;
+
+map<string, Texture> TextureLoad(){
+    map<string, Texture> TextureMap;
+    vector<string> TextureNames = {"Geisha.png", "open hand button.png", "open hand computer.png", "fist button.png","Background.png","capture object button.png","Hand holding the thing.png","object in center.png","computer fist on table.png"};
+
+    for(const string& name : TextureNames){
+        Texture texture;
+        if(texture.loadFromFile("files/" + name)){
+            TextureMap[name] = texture;
+        }else cerr<<"Failed to open files/" + name<<endl;
+    }
+    return TextureMap;
+}
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML Works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Red);
+    RenderWindow window(sf::VideoMode(800, 600), "Konpira");
 
     while(window.isOpen()) {
-        sf::Event event;
+        Event event;
         while(window.pollEvent(event)) {
-            if(event.type == sf::Event::Closed) {
+            if(event.type == Event::Closed) {
                 window.close();
             }
+
         }
 
         window.clear();
-        window.draw(shape);
+
         window.display();
     }
     return 0;
