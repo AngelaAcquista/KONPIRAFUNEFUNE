d6ae226512b3d3e980a85169ecc23128f9af8dee
AngelaAcquista <acquistaangela@gmail.com>
2025-04-21 17:59:58 -0400

    im hungry

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/acqui/CLionProjects/FINAL
+# Install script for directory: C:/Users/acqui/CLionProjects/KONPIRAFUNEFUNE
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "C:/Users/acqui/CLionProjects/FINAL/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/acqui/CLionProjects/KONPIRAFUNEFUNE/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
